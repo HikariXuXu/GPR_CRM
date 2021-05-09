353339f1ba3541d0acceb55271141d664d16fbec
HikariXuXu <hikarixmh@gmail.com>
2021-05-10 01:18:23 +0800

    Update divide_datasets.py

--- divide_datasets.py
+++ divide_datasets.py
@@ -9,6 +9,9 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import ShuffleSplit
+from sklearn.datasets import make_friedman1
+from sklearn.datasets import make_friedman2
+from sklearn.datasets import make_friedman3
 
 ss = StandardScaler()
 cv = ShuffleSplit(n_splits=10, test_size=.1, random_state=0)
@@ -67,4 +70,52 @@ for train_index, test_index in cv.split(servo_std_data):
     test_set.columns = servo_data.columns.values.tolist()
     train_set.to_csv('./Datasets/Servo/servo_train_'+str(count)+'.txt', index=False)
     test_set.to_csv('./Datasets/Servo/servo_test_'+str(count)+'.txt', index=False)
+    count += 1
+
+# Friedman#1 dataset
+X, y = make_friedman1(n_samples=200, noise=1.0, random_state=0)
+friedman1_data = pd.DataFrame(np.column_stack((X, y[:,None])))
+friedman1_data.columns = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10', 'y']
+friedman1_data.to_csv('./Datasets/Friedman#1/friedman#1.data', index=False)
+friedman1_std_data = ss.fit_transform(friedman1_data)
+count = 1
+for train_index, test_index in cv.split(friedman1_std_data):
+    train_set = pd.DataFrame(np.array(friedman1_std_data)[train_index])
+    test_set = pd.DataFrame(np.array(friedman1_std_data)[test_index])
+    train_set.columns = friedman1_data.columns.values.tolist()
+    test_set.columns = friedman1_data.columns.values.tolist()
+    train_set.to_csv('./Datasets/Friedman#1/friedman1_train_'+str(count)+'.txt', index=False)
+    test_set.to_csv('./Datasets/Friedman#1/friedman1_test_'+str(count)+'.txt', index=False)
+    count += 1
+
+# Friedman#2 dataset
+X, y = make_friedman2(n_samples=200, noise=3.0, random_state=0)
+friedman2_data = pd.DataFrame(np.column_stack((X, y[:,None])))
+friedman2_data.columns = ['x1', 'x2', 'x3', 'x4', 'y']
+friedman2_data.to_csv('./Datasets/Friedman#2/friedman#2.data', index=False)
+friedman2_std_data = ss.fit_transform(friedman2_data)
+count = 1
+for train_index, test_index in cv.split(friedman2_std_data):
+    train_set = pd.DataFrame(np.array(friedman2_std_data)[train_index])
+    test_set = pd.DataFrame(np.array(friedman2_std_data)[test_index])
+    train_set.columns = friedman2_data.columns.values.tolist()
+    test_set.columns = friedman2_data.columns.values.tolist()
+    train_set.to_csv('./Datasets/Friedman#2/friedman2_train_'+str(count)+'.txt', index=False)
+    test_set.to_csv('./Datasets/Friedman#2/friedman2_test_'+str(count)+'.txt', index=False)
+    count += 1
+
+# Friedman#3 dataset
+X, y = make_friedman3(n_samples=200, noise=1.0, random_state=0)
+friedman3_data = pd.DataFrame(np.column_stack((X, y[:,None])))
+friedman3_data.columns = ['x1', 'x2', 'x3', 'x4', 'y']
+friedman3_data.to_csv('./Datasets/Friedman#3/friedman#3.data', index=False)
+friedman3_std_data = ss.fit_transform(friedman3_data)
+count = 1
+for train_index, test_index in cv.split(friedman3_std_data):
+    train_set = pd.DataFrame(np.array(friedman3_std_data)[train_index])
+    test_set = pd.DataFrame(np.array(friedman3_std_data)[test_index])
+    train_set.columns = friedman3_data.columns.values.tolist()
+    test_set.columns = friedman3_data.columns.values.tolist()
+    train_set.to_csv('./Datasets/Friedman#3/friedman3_train_'+str(count)+'.txt', index=False)
+    test_set.to_csv('./Datasets/Friedman#3/friedman3_test_'+str(count)+'.txt', index=False)
     count += 1
